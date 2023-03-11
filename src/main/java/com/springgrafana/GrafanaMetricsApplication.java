6203897d0b93e5d5b603269cd51a368ce95878ed
varun <varunkalia@outlook.com>
2023-03-11 22:39:44 +0100

    Initial commit

---
+++ src/main/java/com/springgrafana/GrafanaMetricsApplication.java
@@ -0,0 +1,13 @@
+package com.springgrafana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrafanaMetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrafanaMetricsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springgrafana/GrafanaMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springgrafana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrafanaMetricsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
