d8a307efe420edd4a4f04472def5ed40a1eaac78
varun <varunkalia@outlook.com>
2023-03-12 17:27:11 +0100

    Added first draft with gradle

--- src/main/java/com/springgrafana/controller/GrafanaMetricsController.java
+++ src/main/java/com/springgrafana/controller/GrafanaMetricsController.java
@@ -1,7 +1,9 @@
 package com.springgrafana.controller;
 
 
+import org.json.JSONObject;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +14,8 @@ public class GrafanaMetricsController {
 
     private static final Random random = new Random();
 
-    @GetMapping(path="/memory", produces= MediaType.APPLICATION_JSON_VALUE)
-    public String getRandomMemoryConsumption(){
-        return String.format("%s%s", random.nextInt(1024), "Mb");
+    @GetMapping(path="/memory", produces=MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> getRandomMemoryConsumption(){
+        return ResponseEntity.ok().body(JSONObject.quote(String.format("%s%s", random.nextInt(1024), "Mb")));
     }
 }
