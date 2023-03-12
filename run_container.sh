7079dcfb92e89b590d3c3488c9e91602787713c6
varun <varunkalia@outlook.com>
2023-03-12 15:34:56 +0100

    Added first draft with gradle

--- run_container.sh
+++ run_container.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 #Start grafana
-docker run -d -p 3000:3000 grafana/grafana-enterprise
+docker run -d \
+  -p 3000:3000 \
+  --name=grafana \
+  -e "GF_INSTALL_PLUGINS=https://storage.googleapis.com/plugins-community/grafana-simple-json-datasource/release/1.4.2/grafana-simple-json-datasource-1.4.2.zip" \
+  grafana/grafana-enterprise
 
 #start metrics server
 docker run -p 8080:8080 -d springboot/springboot-grafana-metrics
